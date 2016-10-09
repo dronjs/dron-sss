3482022e30bb6cc9e9d7abb59ceb8b325ce2c286
Vladimir Morulus <vladimirmorulus@gmail.com>
2016-10-09 19:05:19 +0300

    fix installer returned value

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var deployTemplate = require.resolve(path.join(__dirname, 'template/deploy.js'))
  Dron module `dron-vss`*
 */
 function finishDialog() {
-	this.run('confirm', {
+	return this.run('confirm', {
 		question: 'Wanna to run server now?'
 	}).then((answer) => {
 		if (answer) {
@@ -19,7 +19,7 @@ function finishDialog() {
 
 function writeDeployer(config) {
 	return function() {
-		var deployFile = this.touch('deploy');
+		var deployFile = this.touch('deploy.js');
 		deployFile.safeWrite(this.touch(deployTemplate).ejs(config));
 		return finishDialog;
 	}
@@ -29,18 +29,18 @@ function dronVss() {
 	return this.run('prompt', {
 		questions: [
 			{
-				name: 'port',
+				name: 'defaultPort',
 				type: 'string',
 				message: 'What is default server port u like?',
 				default: '1234'
 			}
 		]
 	}).then((answers) => {
-		if (isNaN(answers.port)) {
+		if (isNaN(answers.defaultPort)) {
 			this.warn('Port must be a number between 1 and 9999');
-			return factory;
+			return dronVss;
 		} else {
-			writeDeployer(answers);
+			return writeDeployer(answers);
 		}
 	});
 }
